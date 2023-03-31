d6c83a047d486acacbc619fb63213f6c5089ad95
natanmoon <75587079+natanmoon@users.noreply.github.com>
2023-03-31 11:35:16 -0500

    updates

--- LAB_0/main.py
+++ LAB_0/main.py
@@ -341,14 +341,56 @@ n += 3
 
 
 
-for i in range(100):
+for i in range(50):
+	start_array = [random.randrange(0, 1000) - 1000 for i in range(1000)]
+	final_array = sorted(start_array)
+	start_stats = [0, 0, 0]
+	final_stats = [math.floor(statistics.mean(start_array)), 0 , statistics.multimode(final_array)[0]]
+	final_stats[1] = final_array[len(final_array) // 2 - 1] if len(final_array) % 2 == 0 else final_array[len(final_array) // 2]
+
+	print("\n\rrunning test 7 LOOK: " + str(statistics.mean(start_array)) + " " + str(math.floor(statistics.mean(start_array))))
+	print("input data",start_array)
+	student_code.order(start_array,start_stats)
+	print("student sorted data",start_array)
+	print("expected data", final_array)
+	print("student stats",start_stats)
+	print("expected stats", final_stats)
+	grade=2
+
+	if len(start_array)==len(final_array):
+		tests_passed=tests_passed+1
+		print("size test passed")
+		grade=1
+		for x in range(len(start_array)):
+			if start_array[x]!=final_array[x]:
+				grade=0
+		if grade==1:
+			print("sorted array passed")
+			tests_passed=tests_passed+1
+		else:
+			print("sorted array failed")
+		grade=1
+		for x in range(len(start_stats)):
+			if start_stats[x]!=final_stats[x]:
+				grade=0
+		if grade==1:
+			print("stats passed")
+			tests_passed=tests_passed+1
+		else:
+			print("stats failed")
+	else:
+		print("size test failed")
+	
+	n += 3
+
+for i in range(50):
 	start_array = [random.randrange(0, 2000) - 1000 for i in range(1000)]
 	final_array = sorted(start_array)
 	start_stats = [0, 0, 0]
 	final_stats = [math.floor(statistics.mean(start_array)), 0 , statistics.multimode(final_array)[0]]
 	final_stats[1] = final_array[len(final_array) // 2 - 1] if len(final_array) % 2 == 0 else final_array[len(final_array) // 2]
 
-	print("\n\rrunning test 7")
+	print("\n\rrunning test")
 	print("input data",start_array)
 	student_code.order(start_array,start_stats)
 	print("student sorted data",start_array)
