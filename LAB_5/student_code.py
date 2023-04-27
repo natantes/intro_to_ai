4b4f62b462db1c9c3a1e76125acf12fed253514c
natanmoon <75587079+natanmoon@users.noreply.github.com>
2023-04-27 07:32:58 -0500

    forward checking progress

--- LAB_5/student_code.py
+++ LAB_5/student_code.py
@@ -1,18 +1,8 @@
 import common
-import collections
+import copy
 
 class variables:
 	counter=0
-
-def can_yx_be_z(sudoku, y, x, z):	
-	for i in range(9):
-		if (sudoku[y][i]==z):
-			return False
-		if (sudoku[i][x]==z):
-			return False
-		if(sudoku[int(y/3)*3+int(i/3)][int(x/3)*3+i%3]==z): 
-			return False
-	return True
 
 def board_solved(board):
 	for y, row in enumerate(board):
@@ -24,76 +14,91 @@ def board_solved(board):
 			board[y][x] = temp
 	return True
 
-
-
 def sudoku_backtracking(sudoku):
 	variables.counter = 0
 	def b(board, y, x):
 		if board_solved(board):
 			return True
-		if 0 <= y < 9 and 0 <= x < 9:
-			variables.counter += 1
+		if 0 <= y < 9 and 0 <= x < 9 or x == -1 and y == 0:
 			next_y, next_x = y + ((x + 1) // 9), (x + 1) % 9
-			if board[y][x] == 0:
+			if board[next_y][next_x] == 0:
+				variables.counter += 1
 				for n in range(1, 10):
-					if common.can_yx_be_z(board, y, x, n):
-						board[y][x] = n
+					if common.can_yx_be_z(board, next_y, next_x, n):
+						board[next_y][next_x] = n
 						if b(board, next_y, next_x): return True
-						board[y][x] = 0
+						board[next_y][next_x] = 0
 			else:
 				if b(board, next_y, next_x): return True
 		return False
 
-	b(sudoku, 0, 0)
-	return variables.counter
+	b(sudoku, 0, -1)
+	return variables.counter + 1
+
+def check_domains(domain):
+	for coord in domain:
+		if len(domain[coord]) == 0:
+			return False
+	return True
+
+# def update_domains(sudoku):
+# 	domain = {}
+# 	for i in range(9):
+# 		for j in range(9):
+# 			if sudoku[i][j] == 0:
+# 				temp = []
+# 				for z in range(1,10):
+# 					if common.can_yx_be_z(sudoku, i, j, z):
+# 						temp.append(z)		
+# 				domain[(i,j)] = temp
+# 	return domain
 
 def sudoku_forwardchecking(sudoku):
-    variables.counter = 0
-    domains = [[set(range(1, 10)) for _ in range(9)] for _ in range(9)]
+	variables.counter = 0
 
-    def update_domains(board, y, x, z, remove=True):
-        affected_cells = []
-        for i in range(9):
-            if remove:
-                if z in domains[y][i]:
-                    domains[y][i].remove(z)
-                    affected_cells.append((y, i))
-                if z in domains[i][x]:
-                    domains[i][x].remove(z)
-                    affected_cells.append((i, x))
-            else:
-                domains[y][i].add(z)
-                domains[i][x].add(z)
-            block_y, block_x = int(y / 3) * 3, int(x / 3) * 3
-            for j in range(3):
-                for k in range(3):
-                    if remove:
-                        if z in domains[block_y + j][block_x + k]:
-                            domains[block_y + j][block_x + k].remove(z)
-                            affected_cells.append((block_y + j, block_x + k))
-                    else:
-                        domains[block_y + j][block_x + k].add(z)
-        return affected_cells
+	starting_domain = {}
 
-    def f(board, y, x):
-        if board_solved(board):
-            return True
-        if 0 <= y < 9 and 0 <= x < 9:
-            variables.counter += 1
-            next_y, next_x = y + ((x + 1) // 9), (x + 1) % 9
-            if board[y][x] == 0:
-                for n in domains[y][x]:
-                    if can_yx_be_z(board, y, x, n):
-                        board[y][x] = n
-                        affected_cells = update_domains(board, y, x, n)
-                        if not any(not domains[i][j] for i, j in affected_cells) and f(board, next_y, next_x):
-                            return True
-                        board[y][x] = 0
-                        update_domains(board, y, x, n, remove=False)
-            else:
-                if f(board, next_y, next_x):
-                    return True
-        return False
+	def update_domains(board, y, x, z, domains):
+		for i in range(9):
+			if (y, i) in domains and z in domains[(y, i)]:
+				domains[(y, i)].remove(z)
+			if (i, x) in domains and z in domains[(i, x)]:
+				domains[(i, x)].remove(z)
+			if ((y // 3) * 3 + (i // 3), (x // 3) * 3 + i % 3) in domains and z in domains[((y // 3) * 3 + (i // 3), (x // 3) * 3 + i % 3)]:
+				domains[((y // 3) * 3 + (i // 3), (x // 3) * 3 + i % 3)].remove(z)
+		return domains
 
-    f(sudoku, 0, 0)
-    return variables.counter
+	def f(board, y, x, domain):
+		if board_solved(board):
+			return True
+		if 0 <= y < 9 and 0 <= x < 9:
+			# for row in board:
+			# 	print(row)
+			# print(domain)
+			# print("======================")
+			next_y, next_x = y + ((x + 1) // 9), (x + 1) % 9
+			if board[y][x] == 0:
+				variables.counter += 1
+				for n in range(1, 10):  
+					if common.can_yx_be_z(board, y, x, n):
+						board[y][x] = n
+						new = update_domains(board, y, x, n, copy.deepcopy(domain))
+						# new = update_domains(board)
+						if check_domains(new):
+							if f(board, next_y, next_x, copy.deepcopy(new)):
+								return True
+						board[y][x] = 0
+			else:
+				if f(board, next_y, next_x, copy.deepcopy(domain)): return True
+		return False
+	
+	for y, row in enumerate(sudoku):
+		for x, n in enumerate(row):
+			if sudoku[y][x] == 0:
+				starting_domain[(y, x)] = set() 
+				for z in range(1,10):
+					if common.can_yx_be_z(sudoku, y, x, z):
+						starting_domain[(y, x)].add(z)
+		
+	f(sudoku, 0, 0, starting_domain)
+	return variables.counter + 1
