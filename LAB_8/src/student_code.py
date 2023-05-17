451215b38b02c604d31bb4a743850afb6d887278
natanmoon <75587079+natanmoon@users.noreply.github.com>
2023-05-17 09:09:36 -0500

    stuff

---
+++ LAB_8/src/student_code.py
@@ -0,0 +1,27 @@
+import common
+
+
+def part_one_classifier(data_train, data_test):
+	# PUT YOUR CODE HERE
+	# Access the training data using "data_train[i][j]"
+	# Training data contains 3 cols per row: X in 
+	# index 0, Y in index 1 and Class in index 2
+	# Access the test data using "data_test[i][j]"
+	# Test data contains 2 cols per row: X in 
+	# index 0 and Y in index 1, and a blank space in index 2 
+	# to be filled with class
+	# The class value could be a 0 or a 1
+	return
+
+
+def part_two_classifier(data_train, data_test):
+	# PUT YOUR CODE HERE
+	# Access the training data using "data_train[i][j]"
+	# Training data contains 3 cols per row: X in 
+	# index 0, Y in index 1 and Class in index 2
+	# Access the test data using "data_test[i][j]"
+	# Test data contains 2 cols per row: X in 
+	# index 0 and Y in index 1, and a blank space in index 2 
+	# to be filled with class
+	# The class value could be a 0 or a 8
+	return
